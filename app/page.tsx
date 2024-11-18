3ebbbfd937ce44e67b24b41c3b552c51027898a2
Yonatan Lavie <yonatan.lavie89@gmail.com>
2024-11-18 23:21:21 +0200

    Test

--- app/page.tsx
+++ app/page.tsx
@@ -13,6 +13,7 @@ export default function Home() {
           priority
         />
         <h1>{process.env.TEST}</h1>
+        <h1>{process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}</h1>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
